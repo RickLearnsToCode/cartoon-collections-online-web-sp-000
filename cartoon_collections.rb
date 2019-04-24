9bea2ef543a5d31c2b00d4df5c29c8f291f8c66c
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 13:07:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,14 @@
 def roll_call_dwarves(collection)
-  collection.each_with_index {|elmt, i| puts "#{i+1}. #{elmt}"}# Your code here
+  collection.each_with_index {|elmt, i| puts "#{i+1}. #{elmt}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(collection)
+  i = 0
+  output_array = []
+  while i < collection.length
+    output_array << collection[i].capitalize + "!"
+  end
+  output_array
   # Your code here
 end
 
